48d04fca8d603c61b5734dda14a5cbc2717a0773
dkelvin89 <dkelvin89@outlook.com>
2023-01-09 11:15:58 -0800

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for i in range(0, len(my_string)):
         if my_string[i] != 'C' and my_string[i] != 'c':
             char += my_string[i]
-        return (char)
+    return (char)
