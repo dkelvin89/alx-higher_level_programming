cb7b859b7cedd52bfc00e460c8146f98e0fbb75c
dkelvin89 <dkelvin89@outlook.com>
2023-01-11 08:23:56 -0800

    Task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -8,12 +8,3 @@ def complex_delete(a_dictionary, value):
                 break
 
     return (a_dictionary)
-
-"""
-Write a function that deletes keys with a specific value in a dictionary.
-
-Prototype: def complex_delete(a_dictionary, value):
-If the value doesn’t exist, the dictionary won’t change
-All keys having the searched value have to be deleted
-You are not allowed to import any module
-"""
