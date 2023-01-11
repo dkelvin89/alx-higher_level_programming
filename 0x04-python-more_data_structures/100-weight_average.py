a1333a6ad269d39bb7c44fc7839ce12c98de70e7
dkelvin89 <dkelvin89@outlook.com>
2023-01-11 07:43:15 -0800

    Task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -10,11 +10,3 @@ def weight_average(my_list=[]):
         avg += (tup[0] * tup[1])
         size += tup[1]
     return (avg / size)
-
-"""
-Write a function that returns the weighted average of all integers tuple (<score>, <weight>)
-
-Prototype: def weight_average(my_list=[]):
-Returns 0 if the list is empty
-You are not allowed to import any module
-"""
