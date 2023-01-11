e8170856749d5223834705921b0492f42f88c831
dkelvin89 <dkelvin89@outlook.com>
2023-01-11 07:52:25 -0800

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,13 @@
+"""
+Write a function that returns a key with the biggest integer value.
+
+Prototype: def best_score(a_dictionary):
+You can assume that all values are only integers
+If no score found, return None
+You can assume all students have a different score
+You are not allowed to import any module
+"""
+
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
