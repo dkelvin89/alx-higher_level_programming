e19349f6f72ae176a2d4282ba16ebddca4eb1dfd
dkelvin89 <dkelvin89@outlook.com>
2023-01-11 07:46:05 -0800

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
+    """Converts a roman numeral to an integer."""
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
         return (0)
-    nInt = 0
-    nRom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for i in roman_string:
-        nInt + nRom[i]
-    return (nInt)
 
-"""
-You are not allowed to google anything
-Whiteboard first
-Create a function def roman_to_int(roman_string): that converts a Roman numeral to an integer.
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
+    num = 0
 
-You can assume the number will be between 1 to 3999.
-def roman_to_int(roman_string) must return an integer
-If the roman_string is not a string or None, return 0
-"""
+    for i in range(len(roman_string)):
+        if roman_dict.get(roman_string[i], 0) == 0:
+            return (0)
+
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+                num += roman_dict[roman_string[i]] * -1
+
+        else:
+            num += roman_dict[roman_string[i]]
+    return (num)
