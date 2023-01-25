c65caa323261db9cc8db7d0bc8d6853aceb57bcc
dkelvin89 <dkelvin89@outlook.com>
2023-01-24 17:23:34 -0800

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 1-square.py
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize square's size."""
+        self.__size = size
